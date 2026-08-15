3dd83dfca5ddc97d754f93e480adcabb8c3988e7
pablo.dev.data@gmail.com <pablo.dev.data@gmail.com>
2026-08-15 18:45:09 +0000

    feat/v1 do arquivo

---
+++ test_celulas.py
@@ -0,0 +1,17 @@
+# COMMAND ----------
+
+import pyspark.sql.functions as F
+
+# COMMAND ----------
+
+df = spark.read.table("dados_rh.`2_silver`.silver_funcionarios")
+
+# COMMAND ----------
+
+df_tratado = (df.select(F.col('id'), F.col('funcionario')))
+
+# COMMAND ----------
+
+display(df_tratado)
+
+# COMMAND ----------
