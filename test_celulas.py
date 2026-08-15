8d168d9a82a55097f51fdefe48e1fc2b24ad804a
Pablo Vinicius <147731807+pblovns@users.noreply.github.com>
2026-08-15 16:15:44 -0300

    Add data processing and display in test_celulas.py

--- test_celulas.py
+++ test_celulas.py
@@ -1,14 +1,21 @@
 # Databricks notebook source
+
+# COMMAND ----------
+
+# DBTITLE 1,Importação de Bibliotecas
 import pyspark.sql.functions as F
 
 # COMMAND ----------
 
+# DBTITLE 1,Leitura da Tabela Silver
 df = spark.read.table("dados_rh.`2_silver`.silver_funcionarios")
 
 # COMMAND ----------
 
+# DBTITLE 1,Tratamento de Dados
 df_tratado = (df.select(F.col('id'), F.col('funcionario')))
 
 # COMMAND ----------
 
+# DBTITLE 1,Exibição do Dataframe
 display(df_tratado)
